990a8cb78f712d88f9a7efc404661ee15e2d7b9b
chhaya742 <chhayabagwan34@gmail.com>
2023-03-02 17:52:23 +0530

    chanegs

--- database-config.js
+++ database-config.js
@@ -19,18 +19,12 @@ import "dotenv"
 module.exports = require('knex')({
     client: 'mysql',
     connection: {
-        database: "codeswear",
-        port:3306,
-        user: "yk9gqh0cazxfy7vbmbld",
-        host: "us-east.connect.psdb.cloud",
-        password: "pscale_pw_tBIjg3483FNJLeNaxb7q8SQAfgP7uEkq8n6mFoDtId8",
         ssl: {},
-
-        // host: process.env.NEXT_PUBLIC_DB_HOST,
-        // port: process.env.NEXT_PUBLIC_DB_PORT,
-        // user: process.env.NEXT_PUBLIC_DB_USERNAME,
-        // password: process.env.NEXT_PUBLIC_DB_PASSWORD,
-        // database: process.env.NEXT_PUBLIC_DB_DATABASE,
+        host: process.env.NEXT_PUBLIC_DB_HOST,
+        port: process.env.NEXT_PUBLIC_DB_PORT,
+        user: process.env.NEXT_PUBLIC_DB_USERNAME,
+        password: process.env.NEXT_PUBLIC_DB_PASSWORD,
+        database: process.env.NEXT_PUBLIC_DB_DATABASE,
     }
 });
 
