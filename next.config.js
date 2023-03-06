1ce7f0ceb49ca06734dc52c745c8178ab70f8705
chhaya bagwan <76725368+chhaya742@users.noreply.github.com>
2023-03-06 12:30:32 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api.example.com/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
+
