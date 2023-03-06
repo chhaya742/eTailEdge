47202263fad4764f341a409fcd066d988a2872b3
chhaya bagwan <76725368+chhaya742@users.noreply.github.com>
2023-03-06 13:25:03 +0530

    chnages in next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api',
+        destination: 'http://localhost:3000'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
