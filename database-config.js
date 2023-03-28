1b95d95ce691ce80a76904a7f2f69c43b24abbe1
chhaya bagwan <76725368+chhaya742@users.noreply.github.com>
2023-03-28 12:14:39 +0530

    Update database-config.js

--- database-config.js
+++ database-config.js
@@ -1,35 +1,29 @@
-// import "dotenv"
-// const options = {
-//     client: 'mysql',
-//     connection: {
-//         host: "localhost",
-//         user: "root",
-//         password: "Chhaya@123",
-//         database: "codeswear"
-//     }
-// }
-// const knex = require('knex')(options);
-
-// module.exports=knex
-
-
-
 import "dotenv"
-module.exports = require('knex')({
-    client: 'mysql',
-    connection: {
-        ssl: {},
-         host:"us-east.connect.psdb.cloud",
-        port: "3306",
-        database: "codeswear",
-        username: "0jz77wua1dynoyke5pz1",
-        password: "pscale_pw_MyCSY523ifsrnvonahA2oBNk0ku3RRy5UE0gFFB0bFw"
-
+const mode = "live"
+let options;
+if (mode == "live") {
+    options = {
+        client: 'mysql',
+        connection: {
+            ssl: {},
+            host: "us-east.connect.psdb.cloud",
+            port: "3306",
+            database: "codeswear",
+            username: "0jz77wua1dynoyke5pz1",
+            password: "pscale_pw_MyCSY523ifsrnvonahA2oBNk0ku3RRy5UE0gFFB0bFw"
+        }
     }
-});
+} else {
+    options = {
+        client: 'mysql',
+        connection: {
+            host: 'localhost',
+            user: 'root',
+            password: 'Chhaya@123',
+            database: 'codeswear'
+        }
+    }
+}
 
-
-
-
-
-
+const knex = require('knex')(options);
+module.exports = knex;
