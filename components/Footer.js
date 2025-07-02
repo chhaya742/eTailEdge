b4af90c8a3998e8e926d23d8bdfa760c42258d95
chhaya bagwan <76725368+chhaya742@users.noreply.github.com>
2025-07-02 12:01:18 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -30,79 +30,79 @@ const Footer = () => {
                                 </li>
                             </nav>
                         </div>
-                        <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-                            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">POLICY</h2>
-                            <nav className="list-none mb-10">
-                                <li>
-                                    <a className="text-gray-600 hover:text-gray-800">First Link</a>
-                                </li>
-                                <li>
-                                    <a className="text-gray-600 hover:text-gray-800">Second Link</a>
-                                </li>
-                                <li>
-                                    <a className="text-gray-600 hover:text-gray-800">Third Link</a>
-                                </li>
-                                <li>
-                                    <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
-                                </li>
-                            </nav>
-                        </div>
-                        <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-                            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">HELP</h2>
-                            <nav className="list-none mb-10">
-                                <li>
-                                    <a className="text-gray-600 hover:text-gray-800">First Link</a>
-                                </li>
-                                <li>
-                                    <a className="text-gray-600 hover:text-gray-800">Second Link</a>
-                                </li>
-                                <li>
-                                    <a className="text-gray-600 hover:text-gray-800">Third Link</a>
-                                </li>
-                                <li>
-                                    <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
-                                </li>
-                            </nav>
-                        </div>
-                        <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-                            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">ABOUT</h2>
-                            <nav className="list-none mb-10 ">
-                                <li>
-                                    <a className="text-gray-600 hover:text-gray-800">First Link</a>
-                                </li>
-                                <li>
-                                    <a className="text-gray-600 hover:text-gray-800">Second Link</a>
-                                </li>
-                                <li>
-                                    <a className="text-gray-600 hover:text-gray-800">Third Link</a>
-                                </li>
-                                <li>
-                                    <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
-                                </li>
-                            </nav>
-                        </div>
-                    </div>
-                </div>
-                <div className="bg-gray-100">
-                    <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-                        <p className="text-gray-500 text-sm text-center sm:text-left">© 2020 eTailEdge — Al Rights Reserved
+                //         <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                //             <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">POLICY</h2>
+                //             <nav className="list-none mb-10">
+                //                 <li>
+                //                     <a className="text-gray-600 hover:text-gray-800">First Link</a>
+                //                 </li>
+                //                 <li>
+                //                     <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+                //                 </li>
+                //                 <li>
+                //                     <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+                //                 </li>
+                //                 <li>
+                //                     <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                //                 </li>
+                //             </nav>
+                //         </div>
+                //         <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                //             <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">HELP</h2>
+                //             <nav className="list-none mb-10">
+                //                 <li>
+                //                     <a className="text-gray-600 hover:text-gray-800">First Link</a>
+                //                 </li>
+                //                 <li>
+                //                     <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+                //                 </li>
+                //                 <li>
+                //                     <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+                //                 </li>
+                //                 <li>
+                //                     <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                //                 </li>
+                //             </nav>
+                //         </div>
+                //         <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+                //             <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">ABOUT</h2>
+                //             <nav className="list-none mb-10 ">
+                //                 <li>
+                //                     <a className="text-gray-600 hover:text-gray-800">First Link</a>
+                //                 </li>
+                //                 <li>
+                //                     <a className="text-gray-600 hover:text-gray-800">Second Link</a>
+                //                 </li>
+                //                 <li>
+                //                     <a className="text-gray-600 hover:text-gray-800">Third Link</a>
+                //                 </li>
+                //                 <li>
+                //                     <a className="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                //                 </li>
+                //             </nav>
+                //         </div>
+                //     </div>
+                // </div>
+                // <div className="bg-gray-100">
+                //     <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+                //         <p className="text-gray-500 text-sm text-center sm:text-left">© 2020 eTailEdge — Al Rights Reserved
 
-                        </p>
-                        <span className=" justify-end inline-flex sm:ml-auto sm:mt-0 mt-2 sm:justify-start ">
-                            <a className="text-gray-500">
-                                <FaFacebookF />
-                            </a>
-                            <a className="ml-4 text-gray-500">
-                                <TiSocialLinkedin />
-                            </a>
-                            <a className="ml-4 text-gray-500">
-                                <TiSocialInstagram />
-                            </a>
-                            <a className="ml-4 text-gray-500">
-                                <TiSocialTwitter />
-                            </a>
-                        </span>
-                    </div>
+                //         </p>
+                //         <span className=" justify-end inline-flex sm:ml-auto sm:mt-0 mt-2 sm:justify-start ">
+                //             <a className="text-gray-500">
+                //                 <FaFacebookF />
+                //             </a>
+                //             <a className="ml-4 text-gray-500">
+                //                 <TiSocialLinkedin />
+                //             </a>
+                //             <a className="ml-4 text-gray-500">
+                //                 <TiSocialInstagram />
+                //             </a>
+                //             <a className="ml-4 text-gray-500">
+                //                 <TiSocialTwitter />
+                //             </a>
+                //         </span>
+                //     </div>
                 </div>
             </footer>
         </div>
